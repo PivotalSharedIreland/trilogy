6f8d784ba3f1a20d8811bda2994ec4fa43ce32aa
Brian Butz and Konstantin Semenov <pair+brian+konstantin@pivotallabs.com>
2016-06-17 15:02:10 +0100

    Database helper for tests Remove DB interaction mocks

--- src/test/kotlin/io/pivotal/trilogy/DatabaseHelper.kt
+++ src/test/kotlin/io/pivotal/trilogy/DatabaseHelper.kt
@@ -4,12 +4,15 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource
 import javax.sql.DataSource
 
 object DatabaseHelper {
+    val jdbcUrl = "jdbc:oracle:thin:@192.168.99.100:32769:xe"
+
     fun dataSource(): DataSource {
         return DriverManagerDataSource().apply {
             setDriverClassName("oracle.jdbc.driver.OracleDriver")
-            url = "jdbc:oracle:thin:@192.168.99.100:32769:xe"
+            url = jdbcUrl
             username = "APP_USER"
             password = "secret"
         }
     }
+
 }
--- src/test/kotlin/io/pivotal/trilogy/testrunner/TestCaseRunnerTests.kt
+++ src/test/kotlin/io/pivotal/trilogy/testrunner/TestCaseRunnerTests.kt
@@ -1,36 +1,35 @@
 package io.pivotal.trilogy.testrunner
 
+import io.pivotal.trilogy.DatabaseHelper
 import io.pivotal.trilogy.Fixtures
 import org.jetbrains.spek.api.Spek
 import kotlin.test.expect
 
 
 class TestCaseRunnerTests : Spek({
-    // TODO: externalize the jdbc URL
-    val jdbcUrl = "jdbc:oracle:thin:@192.168.99.100:32769:xe"
 
     it("should run successful test case") {
         val trilogyTestCase = Fixtures.getTestCase("should_pass")
 
-        expect(true) { TestCaseRunner(trilogyTestCase, jdbcUrl).run() }
+        expect(true) { TestCaseRunner(trilogyTestCase, DatabaseHelper.jdbcUrl).run() }
     }
 
     it("should run the failing test case") {
         val trilogyTestCase = Fixtures.getTestCase("should_fail")
 
-        expect(false) { TestCaseRunner(trilogyTestCase, jdbcUrl).run() }
+        expect(false) { TestCaseRunner(trilogyTestCase, DatabaseHelper.jdbcUrl).run() }
     }
 
     it("should run successful test case with a SQL assertion") {
         val trilogyTestCase = Fixtures.getTestCase("should_pass_with_sql")
 
-        expect(true) { TestCaseRunner(trilogyTestCase, jdbcUrl).run() }
+        expect(true) { TestCaseRunner(trilogyTestCase, DatabaseHelper.jdbcUrl).run() }
     }
 
     it("should run failing test case with a SQL assertion") {
         val trilogyTestCase = Fixtures.getTestCase("should_fail_with_sql")
 
-        expect(false) { TestCaseRunner(trilogyTestCase, jdbcUrl).run() }
+        expect(false) { TestCaseRunner(trilogyTestCase, DatabaseHelper.jdbcUrl).run() }
     }
 })
 
